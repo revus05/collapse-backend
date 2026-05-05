35c0c93367d9378e2e6d05bc0052238746edc342
revus05 <skninni05@gmail.com>
2026-05-06 02:02:30 +0300

    fix: fix build

--- src/main/java/com/example/collapse/CollapseApplication.java
+++ src/main/java/com/example/collapse/CollapseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class CollapseApplication {
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(CollapseApplication.class, args);
 	}
 }
